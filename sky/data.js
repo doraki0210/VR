ca4b82c8582bb4185c7ea677aa5e599c7cc4373a
doraki0210 <157569890+doraki0210@users.noreply.github.com>
2024-03-27 15:02:28 +0800

    first

---
+++ sky/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "松菸生態池",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.5534357678279438,
+          "pitch": 1.379608233452208,
+          "title": "生態池",
+          "text": "<span style=\"color: rgb(34, 34, 34); font-family: &quot;Source Sans Pro&quot;; font-size: 17px; background-color: rgb(255, 255, 255);\">原為瑠公圳的蓄水池，在日治時期被松山菸廠用來作為消防、排貯水之用，並種有蓮花兼作觀賞，隨著松山菸廠轉型為文創園區，蓮花池也整頓成為生態池，不再用作消防，但仍有集雨防洪的功效，亦是兩隻名為「松鴨」的大白鴨之家</span>"
+        }
+      ]
+    }
+  ],
+  "name": "松菸生態池",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
